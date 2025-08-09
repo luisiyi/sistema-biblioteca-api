5a5c7fc40a6ff4708d4e0dd1293629c491bfc179
LAGM <luisiyi1997@gmail.com>
2025-08-08 21:37:33 -0700

    Add book method added/updated
    -added "addBook" method, it is as @postMapping and recieve a Producto producto that will be received as the body from the request, then it is saved helpfully for productoServicio and finally returned it.

--- src/main/java/lagm/biblioteca/controlador/BookController.java
+++ src/main/java/lagm/biblioteca/controlador/BookController.java
@@ -5,10 +5,7 @@ import lagm.biblioteca.model.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,11 @@ public class BookController {
         logger.info("Books gotten");
         books.forEach(book -> logger.info(book.toString()));
         return books;
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book){
+        logger.info("Book to add: " + book);
+        return this.bookService.saveBook(book);
     }
 }
