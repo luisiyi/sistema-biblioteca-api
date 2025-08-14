e6941662d7cf684f79ebe08dcb21d7bab80cfc09
LAGM <luisiyi1997@gmail.com>
2025-08-13 21:52:52 -0700

    "getBookById" method added beside the ResourceNotFoundException class
    -added the "getBookById" method in the "BookController" class this method is a "get" (@GetMapping("/books/{id}")) and it will return the Entity/Book according the id passed by the URL, and this is thankfully for the @PathVariable, then we create and initialize and object book type with the "bookService.searchBookById" method if it is found we return with "return ResponseEntity.ok(book);" that is a "200" code if not we throw a new exception with a class created with "404" code. The "reponseEntity" allows in a better way the Http requests, making sure the returning of codes correctly like 200 if exist and 404 if not exist.
    -added the "ResourceNotFoundException" class, this extends from the "RuntimeException" class, also this class has an annotation called @ResponseStatus(value = HttpStatus.NOT_FOUND), this indicate that when an exception is throwed, the server must response with "404" http code (Not Found), also added a constructor that receive the mensaje and it allows throw a message with more details

--- src/main/java/lagm/biblioteca/controlador/BookController.java
+++ src/main/java/lagm/biblioteca/controlador/BookController.java
@@ -1,10 +1,12 @@
 package lagm.biblioteca.controlador;
 
 import lagm.biblioteca.Service.BookService;
+import lagm.biblioteca.excepcion.ResourceNotFoundException;
 import lagm.biblioteca.model.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +33,15 @@ public class BookController {
     public Book addBook(@RequestBody Book book){
         logger.info("Book to add: " + book);
         return this.bookService.saveBook(book);
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable int id){
+        Book book = this.bookService.searchBookById(id);
+        if (book != null){
+            return ResponseEntity.ok(book);
+        }else{
+            throw new ResourceNotFoundException("Not found the id: " + id);
+        }
     }
 }
---
+++ src/main/java/lagm/biblioteca/excepcion/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package lagm.biblioteca.excepcion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
